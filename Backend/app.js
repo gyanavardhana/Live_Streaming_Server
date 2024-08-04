da28b3cd049fa816c2d6b720936dcd0b73b1f882
M. Gyanavardhan <89439095+gyanavardhana@users.noreply.github.com>
2024-08-04 12:40:13 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*,
+    origin: '*',
     methods: ["GET", "POST"]
   }
 });
@@ -22,7 +22,7 @@ const port = 3000;
 
 // Middleware
 app.use(cors({
-  origin: '*,
+  origin: '*',
 }));
 
 app.use(bodyParser.json());
